6e4a9b34a739fd3223aca70cf47471211a6325eb
Carlos Eduardo <cadu8344@gmail.com>
2023-05-15 20:07:25 -0300

    git list

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -26,6 +26,14 @@ public class GameList {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
